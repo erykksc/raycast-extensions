9eff4deb1ee84e3a7baaa09d9ab6e80ea53d94cd
Eryk Ksciuczyk <48735008+erykksc@users.noreply.github.com>
2026-01-07 19:56:13 +0100

    feat: add OpenWith action

--- extensions/fuzzy-file-search/src/search-for-files.tsx
+++ extensions/fuzzy-file-search/src/search-for-files.tsx
@@ -277,6 +277,7 @@ export default function Command() {
             actions={
               <ActionPanel>
                 <Action.Open title="Open" target={filepath} />
+                <Action.OpenWith title="Open With..." path={filepath} />{" "}
                 <Action.ShowInFinder title="Show in Finder" path={filepath} />
                 <Action.CopyToClipboard
                   title="Copy Path to Clipboard"
